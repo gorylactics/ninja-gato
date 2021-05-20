e0e422ce665ec234f6f2a6da07bb0226f664bdc1
Adrian <adrian.miranda.r@gmail.com>
2021-05-20 16:17:40 -0400

    trainee in jQuery. Training exercise

---
+++ assets/js/app.js
@@ -0,0 +1,17 @@
+$(document).ready(function(){
+    $('#img1').click(function(){
+        $('#img1').attr('src' , 'assets/img/ninja0.png')
+    });
+    $('#img2').click(function(){
+        $('#img2').attr('src' , 'assets/img/ninja1.png')
+    });
+    $('#img3').click(function(){
+        $('#img3').attr('src' , 'assets/img/ninja2.png')
+    });
+    $('#img4').click(function(){
+        $('#img4').attr('src' , 'assets/img/ninja3.png')
+    });
+    $('#img5').click(function(){
+        $('#img5').attr('src' , 'assets/img/ninja4.png')
+    });
+});
